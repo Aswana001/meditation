5e2d755cd5e6c71cee803602fa67e54f8b8c29ef
APARNA SAJEEVAN <@parna@2005>
2025-02-09 04:02:38 +0530

    second commit my

--- index.js
+++ index.js
@@ -23,37 +23,37 @@ function sound(key){
 
     switch (key){
         case "Focus": 
-            var audio1 = new Audio("tom-1.mp3");
+            var audio1 = new Audio("beautiful-relaxing-piano-for-videos-239494.mp3");
             audio1.play();   
             break;
         
         case "Love":
-            var audio2= new Audio("tom-2.mp3");
+            var audio2= new Audio("la-valse-de-lune-beautiful-piano-piece-200131.mp3");
             audio2.play();   
             break;
 
         case "Peace":
-            var audio3= new Audio("tom-3.mp3");
+            var audio3= new Audio("rain-and-relaxing-beat-tell-me-why-216570.mp3");
             audio3.play();   
             break;
 
          case "Soul": 
-            var audio4 = new Audio("tom-4.mp3");
+            var audio4 = new Audio("fur-elise-beethoven-216331.mp3");
             audio4.play();   
             break;
 
         case "Present": 
-            var audio5 = new Audio("crash.mp3");
+            var audio5 = new Audio("relaxing-instrumental-music-backsound-11-215393.mp3");
             audio5.play();   
             break;
 
         case "Positive": 
-            var audio6 = new Audio("snare.mp3");
+            var audio6 = new Audio("relaxing-instrumental-music-backsound-11-215393.mp3");
             audio6.play();   
             break;
 
          case "Heal": 
-            var audio7 = new Audio("kick-bass.mp3");
+            var audio7 = new Audio("whip-afro-dancehall-music-110235.mp3");
             audio7.play();   
             break;
           
